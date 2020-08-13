b768f2eafef5bfd67b9d825171fd03820b508b50
RozerinB <rozbaran@icloud.com>
2020-08-13 17:54:15 +0100

    Queue

--- Data-Structures/Queue.py
+++ Data-Structures/Queue.py
@@ -1,1 +1,16 @@
+# initialises queue 
+queue = []
 
+# adding elements to queue 
+queue.append('a')
+queue.append('b')
+queue.append('c')
+print("queue")
+print (queue)
+
+# removing elements from the queue
+print(queue.pop(0))
+print(queue.pop(0))
+
+print ("queue after removing elements")
+print (queue)
