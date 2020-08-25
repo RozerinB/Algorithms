eb2b810102ab2df179e5da95a235610b0386c18b
RozerinB <rozbaran@icloud.com>
2020-08-25 17:12:29 +0100

    Queue.java

---
+++ Data-Structures/Queue.java
@@ -0,0 +1,17 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+
+=======
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queue {
+    public static void main(String[] args) {
+        Queue<String> q = new LinkedkList<>(); 
+
+
+    }
+}
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
